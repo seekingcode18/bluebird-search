f8e5c1788946be3cb424b26759e10e9eb0dc5c2e
Christopher <christopher@seekingwebdev.com>
2019-11-21 12:50:39 +0000

    setup homepage route

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index.ejs'));
+
+app.listen(port, () => console.log(`Bluebird is listening on port ${port}`));
